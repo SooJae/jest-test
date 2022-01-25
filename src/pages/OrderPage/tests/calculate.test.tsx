0710d35e7643ae18f16c166c5f2ca268f9acb4e0
soojae <developersoojae@gmail.com>
2022-01-26 02:27:27 +0900

    fix: test test

--- src/pages/OrderPage/tests/calculate.test.tsx
+++ src/pages/OrderPage/tests/calculate.test.tsx
@@ -86,7 +86,7 @@ describe("total price of goods and options", () => {
     userEvent.type(americaInput, "4");
 
     userEvent.clear(americaInput);
-    userEvent.type(americaInput, "2");
+    userEvent.type(americaInput, "4");
 
     expect(total).toHaveTextContent("1500");
   });
